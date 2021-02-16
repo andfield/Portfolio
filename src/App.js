cb212d9a83c03013380008283fa92c203e7f5526
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-02-16 16:18:58 +1300

    carousel added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/NavBar/navbar'
-
+import MainCarousel from './components/Carousel/Caruosel'
 
 const App=() => {
   return (
     
-    <div className="App">
-      <Navbar />  
+    <div>
+      
+      <Navbar /> 
+      <MainCarousel /> 
 
     </div>
   )
---
+++ src/components/Carousel/Caruosel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Carousel from "react-bootstrap/Carousel"
+import Slide1 from '../../assets/img/carousal/slide1.webp'
+import Slide2 from '../../assets/img/carousal/slide2.webp'
+import './styles.css'
+
+const MainCarousel=() => {
+    return (
+        <div id="home">
+            <Carousel controls={false} indicators interval={2500} pause={false}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 silde-image"
+                        src={Slide1}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 silde-image"
+                        src={Slide2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 silde-image"
+                        src={Slide2}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    )
+}
+
+export default MainCarousel
