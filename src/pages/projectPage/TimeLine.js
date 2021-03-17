76a1384ce6844e9a12e4e6202255d0058dcd1df9
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-03-17 22:17:14 +1300

    FInal changes part 1

--- src/pages/projectPage/TimeLine.js
+++ src/pages/projectPage/TimeLine.js
@@ -1,111 +1,70 @@
 import React from 'react'
-import {Accordion, Card, Image} from 'react-bootstrap'
+import {Col, Button, Row} from 'react-bootstrap'
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import "react-vertical-timeline-component/style.min.css"
 
-import html from "../../assets/img/skills/html-5.svg"
-import css3 from "../../assets/img/skills/css3.svg"
 import react from "../../assets/img/skills/react.svg"
+import mongo from "../../assets/img/skills/mongodb.svg"
 
 import './styles.css'
 
-const ProjectPage=() => {
 
+
+const ProjectPage=() => {
     return (
         <div id="Projects">
             <div className="project">
-                <h1 className="text-center text-white">Hello</h1>
+                <h1 className="text-center text-white">My Work</h1>
                 <VerticalTimeline>
                     <VerticalTimelineElement
-                        className="vertical-timeline-element-work"
-                        contentStyle={{background: 'rgba(0, 0, 0, 0.5)', color: '#fff', boxShadow: '0 4px 8px 0 rgba(255, 187, 0, 0.13), 0 6px 20px 0 rgba(255, 174, 0, 0.123)'}}
+                        className="vertical-timeline-element--work"
+                        contentStyle={{background: 'rgba(0, 0, 0, 0.8)', color: '#fff', boxShadow: '0 4px 8px 0 rgba(255, 187, 0, 0.13), 0 6px 20px 0 rgba(255, 174, 0, 0.123)'}}
                         contentArrowStyle={{borderRight: '15px solid  yellow', marginLeft: '5px'}}
-                        date="2011 - present"
-                        iconStyle={{background: '', color: '#ffff'}}
+                        date="2020"
+                        dateClassName="date"
+                        iconStyle={{background: 'black', color: '#fff'}}
+
                     >
                         <div className="d-flex justify-content-between flex-column mt-1">
-                            <div>
-                                <Accordion>
-                                    <Card>
-                                        <Accordion.Toggle as={Card.Header} eventKey="0" className="p-2 text-center accordian-main">
-                                            Project Details
-                                        </Accordion.Toggle>
+                            <div className="justify-content-center">
 
+                                <Row className="r">
 
-                                        <Accordion.Collapse eventKey="0" className="text-left">
-                                            <Card.Body>
-                                                <strong>Description:</strong> This is a Todo App created with React and Material UI that keeps a track of your Todos
-                        <hr />
-                                                <strong>Features:</strong>
-                                                <ul className="list-styles pt-1">
-                                                    <li>Keeps track of your Todos</li>
-                                                    <li>Powered by React and Material UI</li>
-                                                    <li>Respoisive Design</li>
-                                                </ul>
-                                                <hr />
-                                                <strong>Tech used:</strong>
-                                                <ul>
-                                                    <li>
-                                                        <span className="p-2">
-                                                            <Image
-                                                                src={html}
-                                                                alt="HTML 5"
-                                                                rounded
-                                                                className="image-style m-1"
-                                                            ></Image>{" "}
-                                                              HTML5
-                                                         </span>
-                                                    </li>
-                                                    <li>
-                                                        <span className="p-2">
-                                                            <Image
-                                                                src={css3}
-                                                                alt="CSS 3"
-                                                                rounded
-                                                                className="image-style m-1"
-                                                            ></Image>{" "}
-                                                        CSS3
-                                                        </span>
-                                                    </li>
-                                                    <li>
-                                                        <span className="p-2">
-                                                            <Image
-                                                                src={react}
-                                                                alt="React"
-                                                                rounded
-                                                                className="image-style1 m-1"
-                                                            ></Image>{" "}
-                                                        React
-                                                        </span>
-                                                    </li>
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>React</Button>
 
-                                                </ul>
-                                            </Card.Body>
-                                        </Accordion.Collapse>
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>Mongo</Button>
 
+                                    <Button variant="secondary " size="sm" disabled>express</Button>
+                                </Row>
 
-                                    </Card>
-                                </Accordion>
-                                <div className="d-flex justify-content-between flex-nowrap text-center">
-                                    <a
+                                <Row className="r">
+                                    <strong className="title">Social Mittens</strong>
+                                    <p className="sub-title">A social media platform similar to instagram, developed using MERN stack</p>
+
+                                </Row>
+
+                                <Row className="r">
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark"
+                                        className="mobile-button link-btn "
                                         href="https://akjha96.github.io/Todo-List-React/"
                                         target="_blank"
                                     >
-                                        SEE LIVE
-                </a>
-                                    <a
+                                        See Live
+                                    </Button>
+
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark link-btn"
+                                        className="ml-5 mt-2 mobile-button"
                                         href="https://github.com/akjha96/Todo-List-React"
                                         target="_blank"
                                     >
-                                        SOURCE CODE
-                </a>
-                                    <a
-                                        href="https://www.linkedin.com/posts/anand-kumar-jha-745798a4_react-materialui-fun-activity-6716765686963826688-GIpZ"
-                                        target="_blank"
-                                    >
-                                        WATCH VIDEO
-                </a>
-                                </div>
+                                        GitHub
+                                    </Button>
+                                </Row>
+
                             </div>
                         </div>
 
@@ -113,6 +72,174 @@ const ProjectPage=() => {
 
                     </VerticalTimelineElement>
                 </VerticalTimeline>
+
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{background: 'rgba(0, 0, 0, 0.8)', color: '#fff', boxShadow: '0 4px 8px 0 rgba(255, 187, 0, 0.13), 0 6px 20px 0 rgba(255, 174, 0, 0.123)'}}
+                        contentArrowStyle={{borderRight: '15px solid  yellow', marginLeft: '5px'}}
+                        date="2021"
+                        dateClassName="date"
+                        iconStyle={{background: 'black', color: '#fff'}}
+                        position="right"
+
+                    >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div className="justify-content-center">
+
+                                <Row className="r">
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>HTML5</Button>
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>CSS3</Button>
+
+                                    <Button variant="secondary " size="sm" disabled>Node.JS</Button>
+                                </Row>
+
+                                <Row className="r">
+                                    <strong className="title">Photographer's Portfolio</strong>
+                                    <p className="sub-title">A simple yet beautiful portfolio website designed for an amature photographer using html and css3.</p>
+
+                                </Row>
+
+                                <Row className="r">
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark"
+                                        className="mobile-button link-btn "
+                                        href="https://akjha96.github.io/Todo-List-React/"
+                                        target="_blank"
+                                    >
+                                        See Live
+                                    </Button>
+
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark link-btn"
+                                        className="ml-5 mt-2 mobile-button"
+                                        href="https://github.com/akjha96/Todo-List-React"
+                                        target="_blank"
+                                    >
+                                        GitHub
+                                    </Button>
+                                </Row>
+
+                            </div>
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{background: 'rgba(0, 0, 0, 0.8)', color: '#fff', boxShadow: '0 4px 8px 0 rgba(255, 187, 0, 0.13), 0 6px 20px 0 rgba(255, 174, 0, 0.123)'}}
+                        contentArrowStyle={{borderRight: '15px solid  yellow', marginLeft: '5px'}}
+                        date="2019"
+                        dateClassName="date"
+                        iconStyle={{background: 'black', color: '#fff'}}
+
+                    >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div className="justify-content-center">
+
+                                <Row className="r">
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>Vue.JS</Button>
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>Firebase</Button>
+
+                                    <Button variant="secondary " size="sm" disabled>Vuetify</Button>
+                                </Row>
+
+                                <Row className="r">
+                                    <strong className="title">Smartware Store</strong>
+                                    <p className="sub-title">A fully functioning e-commerce store with admin controls. Designed using Vuetify and developed using Vue and Firebase</p>
+
+                                </Row>
+
+                                <Row className="r">
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark"
+                                        className="mobile-button link-btn "
+                                        href="https://akjha96.github.io/Todo-List-React/"
+                                        target="_blank"
+                                    >
+                                        See Live
+                                    </Button>
+
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark link-btn"
+                                        className="ml-5 mt-2 mobile-button"
+                                        href="https://github.com/akjha96/Todo-List-React"
+                                        target="_blank"
+                                    >
+                                        GitHub
+                                    </Button>
+                                </Row>
+
+                            </div>
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{background: 'rgba(0, 0, 0, 0.8)', color: '#fff', boxShadow: '0 4px 8px 0 rgba(255, 187, 0, 0.13), 0 6px 20px 0 rgba(255, 174, 0, 0.123)'}}
+                        contentArrowStyle={{borderRight: '15px solid  yellow', marginLeft: '5px'}}
+                        date="2021"
+                        dateClassName="date"
+                        iconStyle={{background: 'black', color: '#fff'}}
+                        position="right"
+
+                    >
+                        <div className="d-flex justify-content-between flex-column mt-1">
+                            <div className="justify-content-center">
+
+                                <Row className="r">
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>React</Button>
+
+                                    <Button className="mr-3 " variant="secondary" size="sm" disabled>Mongo</Button>
+
+                                    <Button variant="secondary " size="sm" disabled>express</Button>
+                                </Row>
+
+                                <Row className="r">
+                                    <strong className="title">TCG Dungeon</strong>
+                                    <p className="sub-title">A modern e-commerce store designed using material ui and developed using MERN stack.</p>
+
+                                </Row>
+
+                                <Row className="r">
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark"
+                                        className="mobile-button link-btn "
+                                        href="https://akjha96.github.io/Todo-List-React/"
+                                        target="_blank"
+                                    >
+                                        See Live
+                                    </Button>
+
+                                    <Button
+                                        size="sm"
+                                        variant="outline-dark link-btn"
+                                        className="ml-5 mt-2 mobile-button"
+                                        href="https://github.com/akjha96/Todo-List-React"
+                                        target="_blank"
+                                    >
+                                        GitHub
+                                    </Button>
+                                </Row>
+
+                            </div>
+                        </div>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+
             </div>
 
         </div>
